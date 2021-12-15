7b896c45bddeed37c947d465f340d555f46c3c36
Ebenézer Dorneles <ebenezerdorneles@gmail.com>
2021-12-15 12:25:10 -0300

    criação de usuário

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -11,7 +11,24 @@ module.exports = ()=>{
             })
         }, 
         async store(req, res){
-            
+        //input data
+         const {name, email, password} = req.body;
+         const data = {name, email, password};
+
+         
+         //create 
+         await User.create(data, (err)=> {
+            if(err)
+                return res.status(400).json({
+                    error: true,
+                    message: "Not Create"
+                });
+            else
+                return res.status(200).json({
+                    error: false,
+                    message: "Yes create"
+                });
+         });
         }
     } 
     return HomeController;
